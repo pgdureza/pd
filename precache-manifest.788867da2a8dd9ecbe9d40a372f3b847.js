a3ef80b051b882e879bcded1992cc443016e67b4
Patrick Dureza <pgdureza@gmail.com>
2020-03-27 23:55:06 +0800

    Updates

---
+++ precache-manifest.788867da2a8dd9ecbe9d40a372f3b847.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea51bc469033caf1ba90446107507a10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6aa6096bfda0aab52fb",
+    "url": "/static/css/2.06565635.chunk.css"
+  },
+  {
+    "revision": "df3148cc5671a50eec58",
+    "url": "/static/css/main.bbbc82d8.chunk.css"
+  },
+  {
+    "revision": "b6aa6096bfda0aab52fb",
+    "url": "/static/js/2.7cb116d0.chunk.js"
+  },
+  {
+    "revision": "e3684e1c21142b1c7d1831e5ab8942c5",
+    "url": "/static/js/2.7cb116d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3148cc5671a50eec58",
+    "url": "/static/js/main.ea2c5e02.chunk.js"
+  },
+  {
+    "revision": "f23110f07040846bcf19",
+    "url": "/static/js/runtime-main.543573e3.js"
+  },
+  {
+    "revision": "8de631fd071cc1a1ea197a67c590e012",
+    "url": "/static/media/background.8de631fd.jpg"
+  },
+  {
+    "revision": "82aaf831072fca6c8e19c6cfd06d4c41",
+    "url": "/static/media/sitting.82aaf831.svg"
+  },
+  {
+    "revision": "f17fdee9a01631dbdc05d58f486a8941",
+    "url": "/static/media/skills.f17fdee9.svg"
+  }
+]);
